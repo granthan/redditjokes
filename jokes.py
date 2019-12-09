6c30d999e7590c6b2d807161fdabd198a101c61f
Grant Han <granthan@berkeley.edu>
2019-12-09 01:28:38 -0800

    Updates to subreddit parsing

--- jokes.py
+++ jokes.py
@@ -3,14 +3,21 @@ import sys, time, csv, requests, json
 def run_jokes():
     arguments_len = len(sys.argv)
 
-    if arguments_len == 1:
-        joke_list = read_jokes_reddit('dadjokes')
-    else:
-        joke_list = read_jokes_csv(sys.argv[1])
+    print("Example subreddits: jokes, dadjokes, puns")
+    inp = input("Specify subreddit or type 'exit' to quit: ")
+    
+    if inp == 'exit':
+        exit()
+
+    joke_list = read_jokes_reddit(inp)
+
+    if not joke_list:
+        print("Invalid jokes subreddit\n")
+        run_jokes()
 
     print_jokes(joke_list[0], joke_list[1])
 
-    continue_read = read_input();
+    continue_read = read_input()
     curr_index = 2
 
     while curr_index < len(joke_list) and continue_read:
@@ -32,21 +39,32 @@ def print_jokes(prompt, punchline):
 
 def read_input():
     while True:
-        inp = input("Type 'next' for next joke, or 'quit' to exit: ")
+        inp = input("Type 'next' for next joke, or 'exit' to quit: ")
         if inp == 'next':
             return True
-        elif inp == 'quit':
+        elif inp == 'exit':
             return False
         else:
             print("\n" + "I don't understand, please try again.")
 
 
 def read_jokes_reddit(subreddit):
-    reddit_json_link = "https://www.reddit.com/r/" + subreddit + ".json";
+    reddit_json_link = "https://www.reddit.com/r/" + subreddit + ".json"
     reddit_request = requests.get(reddit_json_link, headers = {'User-agent': "Grant's joke bot 1.0"})
     parsed_jokes = json.loads(reddit_request.text)
+
+    if 'data' not in parsed_jokes:
+        print("Invalid jokes subreddit\n")
+        run_jokes()
+
     jokes_list = parsed_jokes['data']['children']
+
+    if not jokes_list:
+        print("Invalid jokes subreddit\n")
+        run_jokes()
+    
     filtered_jokes = []
+    
     for joke in jokes_list:
         over_18 = joke['data']['over_18']
         joke_title = joke['data']['title']
@@ -60,7 +78,7 @@ def read_jokes_reddit(subreddit):
 
 def read_jokes_csv(jokes_csv):
     jokes_list = []
-    current_line = 1;
+    current_line = 1
     try:
         with open(jokes_csv) as jokes_file:
             jokes_reader = csv.reader(jokes_file, delimiter=',')
@@ -69,8 +87,8 @@ def read_jokes_csv(jokes_csv):
                     prompt, punchline = joke[0], joke[1]
                     if not prompt or not punchline:
                         raise ValueError()
-                    jokes_list.append(prompt);
-                    jokes_list.append(punchline);
+                    jokes_list.append(prompt)
+                    jokes_list.append(punchline)
                 else:
                     raise ValueError()
                 current_line += 1
